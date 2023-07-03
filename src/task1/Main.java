7e794d1dd666172ae3e941a67fc59fc0570c4573
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-07-03 22:46:59 +0300

    Solution task 1

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,8 +1,26 @@
 package task1;
 
 public class Main {
+
+    private final static String[] incorrectStr = {"brange", "plum", "tomato", "onibn", "grape"};
+
     public static void main(String[] args) {
+    printStrArray(correctBtoO(incorrectStr));
+    }
+    private static String[] correctBtoO (String[] incorrectStr) {
+        int length = incorrectStr.length;
+        String[] correctStr = new String[length];
+        for (int i = 0; i < length; i++) {
+            correctStr[i] = incorrectStr[i].replace("b", "o");
+        }
+        return correctStr;
+    }
+    private static void printStrArray(String[] array) {
+        int count = 1;
+        for (String name : array) {
+            System.out.println(count + ") " + name);
+            count++;
+        }
 
     }
-
 }
